757f17678a57ea418b0044cac7c86fd7aec570f1
AlcaideUOC <alcaide@uoc.edu>
2024-12-20 10:56:43 +0100

    cambios

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,28 @@
-// Obtener los elementos del DOM
+// Extraigo del DOM:
 const menuToggle = document.getElementById('menu-pequeno');
 const menuItems = document.getElementById('menu-items');
 
-// Evento de clic en el botón hamburguesa
+// Si hay clic en el botón hamburguesa
 menuToggle.addEventListener('click', () => {
-    menuItems.classList.toggle('active'); // Muestra/oculta el menú
+    menuItems.classList.toggle('active'); // Muestra o bien oculta el menú
     menuToggle.innerHTML = menuItems.classList.contains('active') 
-        ? '&#x2715;' // Ícono de cerrar (✕)
-        : '&#9776;'; // Ícono hamburguesa (☰)
+        ? '&#x2715;' // Pongo el icono de cerrar
+        : '&#9776;'; // Pongo el icono hamburguesa
 });
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Seleccionar todos los botones con el atributo data-section
+    // Selecciono todos los botones con el atributo data-section
     const botones = document.querySelectorAll("button[data-section]");
   
     botones.forEach(boton => {
       boton.addEventListener("click", () => {
-        // Obtener el valor del atributo data-section
+        // Obtengo el valor del atributo data-section
         const sectionId = boton.getAttribute("data-section");
         const section = document.getElementById(sectionId);
   
         if (section) {
-          // Desplazamiento suave hacia la sección
+          // Hace un desplazamiento suave hacia la sección
           section.scrollIntoView({
             behavior: "smooth",
             block: "start"
@@ -31,8 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   });
-
-
 
   document.addEventListener("DOMContentLoaded", () => {
     const botones = document.querySelectorAll(".boton-nav");
